0a7159d63786c89c1a02a694f7bbe6ac2774a969
SukHwan Kim <30820468+jerry4897@users.noreply.github.com>
2018-08-07 05:23:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from PyQt4.QtGui import *
 import dropped_blocks
 import arrow
 import menu
-global qd
-global wd
 
+#####
+click_listen = None
+num = 0
+#####
+
+glo = None
 class pallete_part(QGraphicsObject):                        # pallete part : right corner of Window.         
     def __init__(self, parent=None):
         super(pallete_part, self).__init__(parent)
@@ -115,14 +119,10 @@ class qgraphicsView(QGraphicsView):                     # Main board Graphic Vie
         
         pos = event.pos()
         #pal = dropped_blocks.graphics(pos, int(event.mimeData().text()))                # Drop(Add) on the graphics
-        dropped_blocks.graphics(pos, int(event.mimeData().text()), self.scene)
-        #dropped_blocks.graphics(pos, int(event.mimeData().text()))
         #dropped_blocks.graphics(pos, int(event.mimeData().text()), self.scene)
-        #tmp = QPoint(200, 200)
-        #dropped_blocks.arrows(pos, tmp, self.scene)
-        ###self.scene.addItem(pal)
-        #dropped_blocks.arrow(self.scene)
-        #self.scene.addItem(dropped_blocks.click_listener().paintEvent(self))
+
+        graphics(pos, int(event.mimeData().text()), self.scene)
+
         event.acceptProposedAction()
 
     def resizeEvent(self, event):
@@ -141,7 +141,7 @@ class Dock_Graphics(QDockWidget):                               # Graphics (Drop
         self.setWidget(self.graphic)
         self.show()
     def mousePressEvent(self, event):
-        dropped_blocks.click_listen.flag = 0
+        click_listen.flag = 0
         print("Click empty screen")
 
 class Dock_Code(QDockWidget):                                   # Code (Code writing Zone) Dock widget
@@ -160,23 +160,14 @@ class Dock_Constraints(QDockWidget):
     def __init__(self):
         super(Dock_Constraints, self).__init__()
         self.setWindowTitle('Constraints')
-        self.setWidget(menu.menu_init(0))
+        #self.setWidget(menu.menu_init())
         self.show()
-'''class DockContents(QWidget):                                                # delete?
-    _sizehint = None
-    def setSizeHint(self, width, height):
-        self._sizehint = QSize(width, height)
 
-    def sizeHint(self):
-        print('sizeHint:', self._sizehint)
-        if self._sizehint is not None:
-            return self._sizehint
-        return super(MyWidget, self).sizeHint()
-'''
 class Window(QMainWindow):                                                 # Main window
     def __init__(self):
         QMainWindow.__init__(self)
-
+        global click_listen
+        global dock_cons
         pallete_buttons = pallete()                                         
 
         scene = QGraphicsScene(0, 0, 200, 400)
@@ -191,16 +182,138 @@ class Window(QMainWindow):                                                 # Mai
         
         self.dock1 = Dock_Code()
         self.addDockWidget(Qt.BottomDockWidgetArea, self.dock1)
-
+        
         self.dock2 = Dock_Constraints()
         self.addDockWidget(Qt.BottomDockWidgetArea, self.dock2)
+        dock_cons = self.dock2
 
-        dropped_blocks.click_listen = dropped_blocks.click_listener()             #
+        click_listen = click_listener()             #
+
+############################################################################################
+
+class graphics_part(QGraphicsObject):                                               #
+    def __init__(self, parent=None):
+        super(graphics_part, self).__init__(parent)
+        global num
+        self.color = QColor(Qt.lightGray)
+        self.dragOver = True
+        self.pos_ = None
+        self.setAcceptDrops(False)
+        self.setFlag(self.ItemIsMovable, False)
+        self.index = num
+        self.connect_list = []                                                      # List that save connected blocks
+        self.table = menu.menu_block(self.index, self.shape, self.connect_list)
+        self.table.itemChanged.connect(self.refresh_table)
     
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:                                         # drag & drop
+            self.setCursor(Qt.ClosedHandCursor)
+            self.table.setItem(0, 1, QTableWidgetItem(str(self.shape)))
+            print(str(self.index) + " " + str(self.shape) + " " + str(self.connect_list))
+            window.dock2.setWidget(self.table)
+            self.setFlag(self.ItemIsMovable, True)
+
+            if(click_listen.flag == 0):                                             # 0 : click background
+                click_listen.from_ = self                                           # 1 : click block    
+                click_listen.flag = 1                                               # clock blocks consequently : connect blocks
+            elif(click_listen.flag == 1):
+                click_listen.to_ = self
+                click_listen.connect(self)
+                arrow.arrows(click_listen.from_, click_listen.to_)
+                click_listen.flag = 0
+                click_listen.from_ = None
+                click_listen.to_ = None
+
+        elif event.button() == Qt.RightButton:                                      # remove
+            print("right clicked")
+            #tb = menu.TableWidget()
+            #tb.show()
+            #tb = menu.mytest()
+            #tb.show()
+            #self.update()
+            self.setParent(None)
+            self.update()
+
+    def refresh_table(self):
+        self.index = self.table.item(0,0).text()
+        self.shape = self.table.item(0,1).text()
+        self.connect_list.append(self.table.item(0,2).text())
+
+class neuron_rec_(graphics_part):
+    def __init__(self, pos_):
+        super(neuron_rec_, self).__init__()
+        self.setCursor(Qt.OpenHandCursor)
+        self.shape = 1
+
+        self.pos_ = pos_
+    def boundingRect(self):
+        return QRectF(self.pos_.x() - 30, self.pos_.y() - 60, 60, 60)
+
+    def paint(self, painter, option, widget=None):
+        painter.setBrush(self.color.lighter(130) if self.dragOver else self.color)
+        painter.drawRect(self.pos_.x() - 30, self.pos_.y() - 60, 60, 60)
+
+class neuron_cir_(graphics_part):
+    def __init__(self, pos_):
+        super(neuron_cir_, self).__init__()
+        self.setCursor(Qt.OpenHandCursor)
+        self.shape = 2
+
+        self.pos_ = pos_
+    def boundingRect(self):
+        return QRectF(self.pos_.x()-20, self.pos_.y()-60, 40, 60)
+    def paint(self, painter, option, widget=None):
+        painter.setBrush(self.color.lighter(130) if self.dragOver else self.color)
+        painter.drawEllipse(self.pos_.x() - 20, self.pos_.y() - 60, 40, 60)
+
+
+class graphics(graphics_part):
+    def __init__(self, pos, shape, scene):
+        super(graphics, self).__init__()
+        self.setFlag(self.ItemHasNoContents)
+        self.scene = scene
+        global num
+
+        if(shape == 1):
+            print("Add Rectangle")
+            self.scene.addItem(neuron_rec_(pos))
+        else:
+            print("Add Circle")
+            self.scene.addItem(neuron_cir_(pos))
+
+        num += 1                                                        # index number for blocks
+
+    def boundingRect(self):
+        return QRectF()
+
+    def paint(self, painter, option, widget=None):
+        pass
+
+class click_listener():
+    global num
+    def __init__(self):
+        super(click_listener, self).__init__()
+        self.flag = 0
+        self.from_ = None
+        self.to_ = None
+
+    def connect(self, dest):
+        flag = 0                                                    # Avoid duplication
+        for i in self.from_.connect_list:
+            if i == dest.index:
+                flag = 1
+                break
+
+        if flag == 0:
+            self.from_.connect_list.append(dest.index)
+
+        print("dest : ", self.from_.index)
+        print(self.from_.connect_list)
+
 if __name__ == '__main__':
+    global window
     app = QApplication(sys.argv)
     window = Window()
-    #neu_list = dropped_blocks.neuron_list()
     window.setGeometry(500, 200, 1000, 700)
     window.setWindowTitle('Mango')
     window.show()
